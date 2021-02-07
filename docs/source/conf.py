ad8011e5bca9300cbe38e4b6827cdaebeb378563
Juho Kim <juho-kim@outlook.com>
2021-02-07 04:31:18 -0500

    Added documentations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'sigmage'
@@ -24,7 +23,6 @@ author = 'Juho Kim et al.'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1.dev4'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -42,7 +40,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
